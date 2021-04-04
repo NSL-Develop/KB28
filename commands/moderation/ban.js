73c771096ff73e6378a7fdffd1731783d6dfd6b1
NSL-Develop <72661866+NSL-Develop@users.noreply.github.com>
2021-04-04 16:37:31 +0200

    feat: add ban command

---
+++ commands/moderation/ban.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const index = require('../../index.js');
+const client = index.client
+
+const prefix = index.prefix
+const readJson = index.readJson
+const writeJson = index.writeJson
+const existsFile = index.existsFile
+const makeDir = index.makeDir
+
+client.on('message', msg => {
+    const prefix = index.prefix
+    const args = msg.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    if (msg.guild != null) {
+        if (command == 'ban' && msg.member.hasPermission('BAN_MEMBERS')) {
+            if (msg.mentions.users.first() && typeof(msg.mentions.users.first()) != "undefined") {
+                msg.guild.member(msg.mentions.users.first()).ban()
+                const banEmbed = new Discord.MessageEmbed()
+                .setTitle(`User Banned`)
+                .addFields(
+                    {'name' : 'Informations','value' : `${msg.mentions.users.first()} has been banned.`})
+                msg.channel.send(banEmbed)
+            };
+        };
+    };
+});
