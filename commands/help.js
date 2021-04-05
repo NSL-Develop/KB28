0cc8fe7d5008f69ee6e480d5223c279ecbc70720
NSL-Develop <72661866+NSL-Develop@users.noreply.github.com>
2021-04-05 18:13:33 +0200

    Feat : Add delete-command to help

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ client.on('message', msg => {
             const helpEmbed = new Discord.MessageEmbed()
             .setTitle(`All Commands`)
             .addFields(
-                {'name' : 'Setup','value' : `${prefix}create-command : Create A New Custom Command.`},
+                {'name' : 'Setup','value' : `${prefix}create-command : Create A New Custom Command.\n${prefix}delete-command : Delete A Custom Command.`},
                 {'name' : 'Informations','value' : `${prefix}help : Display All Commands Help.\n${prefix}display-avatar : Displays The Avatar Of The Mentioned Member.`},
                 {'name' : 'Moderation','value' : `${prefix}ban : Ban The Mentioned Member.\n${prefix}kick : Kick The Mentioned Member.\n${prefix}mute : Mute The Mentioned Member.`})
 			msg.channel.send(helpEmbed)
