65cb012e3e06ec2e34bbcf93235bf685bbbd2d94
NSL-Develop <72661866+NSL-Develop@users.noreply.github.com>
2021-04-05 16:18:41 +0200

    Feat : Add delete-command command

--- index.js
+++ index.js
@@ -37,5 +37,6 @@ const displayAvatarCmd = require("./commands/members/display-avatar.js");
 
 const customCommandsManager = require("./commands/server/custom-commands-manager.js");
 const createCommandCmd = require("./commands/server/create-command.js");
+const deleteCommandCmd = require("./commands/server/delete-command.js");
 
 client.login(readJson("config.json").token);
