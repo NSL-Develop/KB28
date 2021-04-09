4eedd0987817ef39dfe7c2b6e162d7b9b01e12a9
NSL-Develop <nsl-develop@outlook.com>
2021-04-09 14:37:34 +0200

    Feat : Add server events command to help

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ client.on('message', msg => {
             const helpEmbed = new Discord.MessageEmbed()
             .setTitle(`All Commands`)
             .addFields(
-                {'name' : 'Setup','value' : `${prefix}create-command : Create A New Custom Command.\n${prefix}delete-command : Delete A Custom Command.`},
+                {'name' : 'Setup','value' : `${prefix}create-command : Create A New Custom Command.\n${prefix}delete-command : Delete A Custom Command.\n${prefix}set-welcome-channel : Set welcome messages channel.\n${prefix}set-leave-channel : Set leave messages channel.`},
                 {'name' : 'Informations','value' : `${prefix}help : Display All Commands Help.\n${prefix}display-avatar : Displays The Avatar Of The Mentioned Member.`},
                 {'name' : 'Moderation','value' : `${prefix}ban : Ban The Mentioned Member.\n${prefix}kick : Kick The Mentioned Member.\n${prefix}mute : Mute The Mentioned Member.`})
 			msg.channel.send(helpEmbed)
