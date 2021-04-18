e8c28145bdec6e6d5449a3ae7a1ea40018d89772
NSL-Develop <72661866+NSL-Develop@users.noreply.github.com>
2021-04-18 11:07:49 +0200

    Feat : Add new command to help

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,8 @@ client.on('message', msg => {
             .addFields(
                 {'name' : 'Setup','value' : `${prefix}create-command : Create A New Custom Command.\n${prefix}delete-command : Delete A Custom Command.\n${prefix}set-welcome-channel : Set welcome messages channel.\n${prefix}set-leave-channel : Set leave messages channel.`},
                 {'name' : 'Informations','value' : `${prefix}help : Display All Commands Help.\n${prefix}display-avatar : Displays The Avatar Of The Mentioned Member.`},
-                {'name' : 'Moderation','value' : `${prefix}ban : Ban The Mentioned Member.\n${prefix}kick : Kick The Mentioned Member.\n${prefix}mute : Mute The Mentioned Member.`})
+                {'name' : 'Moderation','value' : `${prefix}ban : Ban The Mentioned Member.\n${prefix}kick : Kick The Mentioned Member.\n${prefix}mute : Mute The Mentioned Member.`},
+                {'name' : 'Roles','value' : `${prefix}add-role : Give a role to a member.\n${prefix}remove-role : Remove a role from a member.`})
 			msg.channel.send(helpEmbed)
         };
     };
